1a744ec50945cde63e5c1ec600a309bdaf0e24e8
isheetas <isheeta_sinha@hotmail.com>
2025-05-03 22:19:46 -0400

    Add CoT to system prompt

--- Code/prompts/system_prompts.py
+++ Code/prompts/system_prompts.py
@@ -5,13 +5,29 @@ SYSTEM_PROMPTS  = {
     "disease_entity_extraction": 
     """You are an expert disease entity extractor from a sentence and report it as JSON in the following format:
     diseases: <List of extracted entities>
-    Please report only Diseases. Do not report any other entities like Genes, Proteins, Enzymes etc.""",
+    Please report only Diseases. Do not report any other entities like Genes, Proteins, Enzymes etc.
+    """,
 
     "mcq_question":
     """You are an expert biomedical researcher. For answering the Question at the end, you need to first read the Context provided.
     Based on that Context, provide your answer in the following JSON format for the Question asked.
     "answer": <correct answer>
-    """
+    """,
+    
+    "mcq_question_cot_1":
+    """You are an expert biomedical researcher. Based on that Context provided, provide your answer in the following JSON format for the Question asked. The field "answer" is a the correct answer.
+    The field "reasoning" contains 4-5 sentences explaining step-by-step reasoning of how the answer was derived and why the other options are not suitable answers.
+    "answer": <correct answer>, "reasoning": <reasoning>
+    """,
+    
+    "mcq_question_cot_2":
+    """You are an expert biomedical researcher. Based on that Context provided, provide your answer in the following JSON format for the Question asked. The field "answer" is a the correct answer.
+    The field "reasoning" contains sentences explaining step-by-step reasoning of how the answer was derived. Here is an example of question and its corresponding reasoning,
+    the question is  "Out of the given list, which of the following conditions is least likely to be an effect of a mutation in the FLG Gene? Given list: Dermatitis, Esophagitis, Dyspepsia, Asthma", and the
+    reasoning "From the given data, the FLG gene shows strong associations with Dermatitis and Asthma, indicating a clear link. Esophagitis has a lower Z-score, suggesting a weaker association. Dyspepsia is not listed at all, implying no significant known association. Since all other conditions have at least some association with FLG, however weak, Dyspepsia stands out as the least likely to be an effect of a mutation in the FLG gene."
+    
+    "answer": <correct answer>, "reasoning": <reasoning>
+    """,
 }
 
 def get_system_prompt(task: str) -> str:
--- Code/test.py
+++ Code/test.py
@@ -8,15 +8,20 @@ from utils.schema_loader import load_task_schema
 openai_client = OpenAIClient()
 spoke_api_client = SpokeAPIClient()
 
-question = "Are there any drugs used for weight management in patients with Bardet-Biedl Syndrome?"
+# question = "Are there any drugs used for weight management in patients with Bardet-Biedl Syndrome?"
+# question = "Out of the given list, which Gene is associated with head and neck cancer and uveal melanoma. Given list is: ABO, CACNA2D1, PSCA, TERT, SULT1B1"
+# question = "Out of the given list, which Gene is associated with herpes zoster and psoriatic arthritis. Given list is: HLA-B, ADGRV1, CPS1, SULT1B1, ATG5"
+question = "Out of the given list, which Variant is associated with collagenous colitis and autoimmune hepatitis. Given list is: rs2187668, rs6426833, rs6969780, rs3787184, rs230540"
+# question = "Out of the given list, which of the following conditions is least likely to be an effect of a mutation in the FLG Gene? Given list: Dermatitis, Esophagitis, Dyspepsia, Asthma"
 
 rag = RAG(openai_client, spoke_api_client, config.CONTEXT_VOLUME, config.QUESTION_VS_CONTEXT_SIMILARITY_PERCENTILE_THRESHOLD, config.QUESTION_VS_CONTEXT_MINIMUM_SIMILARITY)
 context = rag.retrieve(question)
 
 enriched_prompt = "Context: "+ context + "\n" + "Question: "+ question
-system_prompt = get_system_prompt(task="mcq_question")
-text_data = load_task_schema(task="mcq_question")
+system_prompt = get_system_prompt(task="mcq_question_cot")
+text_data = load_task_schema(task="mcq_question_cot")
 output = openai_client.generate_json_response(instructions=system_prompt, input_text=enriched_prompt, text_data=text_data)
 
 print(enriched_prompt)
+print("With reasoning")
 print(output)
